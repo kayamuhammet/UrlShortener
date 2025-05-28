eccf104e70572cd9d010b42dce14b4669ef6e029
Muhammet Kaya <66886314+kayamuhammet@users.noreply.github.com>
2025-05-28 23:00:29 +0300

    first commit

---
+++ UrlShortener/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortener.Controllers;
+
+public class HomeController : Controller
+{
+
+    public HomeController()
+    {
+        
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
